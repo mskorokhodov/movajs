c7f61965e57cad743fe7bea37207e218d1786ffe
Michael Skorokhodov <mskorokh@cisco.com>
2019-12-06 10:28:28 -0800

    make store exportable

--- index.js
+++ index.js
@@ -3,3 +3,4 @@ import 'construct-style-sheets-polyfill';
 export { html } from './lib/html';
 export { css } from './lib/css';
 export { Element } from './lib/element';
+export { store } from './lib/store';
