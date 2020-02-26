56de30b0bdb9fd3d68f1484e36c7a1ed9b1017b6
Michael Skorokhodov <mskorokh@cisco.com>
2020-02-26 22:29:08 +0200

    exports fix

--- index.js
+++ index.js
@@ -1,6 +1,1 @@
-// import 'construct-style-sheets-polyfill';
-
-export { html } from './lib/html';
-export { css } from './lib/css';
-export { Element } from './lib/old/element';
-export { createStore } from './lib/old/store';
+export * from './lib'
